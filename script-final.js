c789ae08bd115a1bb78ef8214955ceb0023d91f0
Melanie Simpson <Melanie67@gmail.com>
2022-10-05 18:26:50 -0400

    script final.js mods

--- script-final.js
+++ script-final.js
@@ -13,12 +13,29 @@ function playRound(){
 
 function playerChoice (){
        let input = prompt('Enter rock, paper, or scissors');
-       console.log(input);
+       while (input == null){
+        input = prompt('Remember to enter rock, paper, or scissors,');
+       }
+       input = input.toLowerCase();
+       let check = validateInput(input);
+
+       //Reinstate this code under 'let check' if this update does not work
+       if(check == true){
+        console.log(input);
+       }
+       //console.log(input);
     }    
     
 function computerChoice(){
     return choices[Math.floor(Math.random() * 3)]
 }
 
-game();
+function validateInput(playerInput){
+    if(choices.includes(playerInput)){
+        return true;
+    } else{
+        return false;
+    }
 
+}
+game();
